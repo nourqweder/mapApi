86369819bf6b1db1ac0d61e683a3aef7ba3650b3
Nour Elhouda Qweder <nouqw898@student.liu.se>
2019-10-18 11:29:54 +0200

    add complete function

---
+++ R/complete.R
@@ -0,0 +1,21 @@
+#'@title complete
+#'  @description
+#' @param 'workdir' is a character vector of length 1 indicating
+#'  the location of the CSV files
+#'  @param 'colName' is a character vector of length 1 indicating
+#'    mean; either "sulfate" or "nitrate"
+#'
+#'@Return a data frame of the from:
+#'  id nobs
+#'  1  117
+#'  2  1041
+#'  where nobs is: number of complete cases
+#'  and id is: mointor id
+#' @example complete("specdata", 1)
+complete <- function(workdir, numberOfRows = 1) {
+
+
+
+
+
+}
